5de1725e2eb213085039c800cdac9ecc69b9cbca
AlexRatik <aleksandr-ratkevich@mail.ru>
2022-06-19 22:14:26 +0300

    feat: create server

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import http, { IncomingMessage, ServerResponse } from "node:http";
+import "dotenv/config";
+import { appService } from "./services/appService";
+
+const server = http.createServer(
+    (req: IncomingMessage, res: ServerResponse) => {
+        appService(req, res);
+    }
+);
+
+const PORT = process.env.PORT || 8080;
+const { pid } = process;
+server.listen(PORT, () => {
+    console.log(`server is listening on ${PORT}, process id: ${pid}`);
+});
